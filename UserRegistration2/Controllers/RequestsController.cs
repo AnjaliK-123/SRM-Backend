e4d2fb5782727ed7529b085e0dc5bb95d281939d
Mayuresh <mayuresh.warade@gmail.com>
2020-08-10 19:18:58 +0530

    Recoverd

--- UserRegistration2/Controllers/RequestsController.cs
+++ UserRegistration2/Controllers/RequestsController.cs
@@ -63,26 +63,84 @@ namespace UserRegistration2.Controllers
         // PUT: api/Requests/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for
         // more details see https://aka.ms/RazorPagesCRUD.
-    /*    [HttpPut("{id}")]
-        public ActionResult UpdateRequest(int Id, RequestUpdateDto updateRequest)
-        {
-           
-            var updateRequestFromRepo = _repository.GetRequestById(Id);
-            if (updateRequestFromRepo == null)
+        /*    [HttpPut("{id}")]
+            public ActionResult UpdateRequest(int Id, RequestUpdateDto updateRequest)
             {
-                return NotFound();
-            }
-            _mapper.Map(updateRequest, updateRequestFromRepo);
-            _repository.UpdateRequest(updateRequestFromRepo);
-            _repository.SaveChanges();
-            return NoContent();
-        }*/
 
-       
+                var updateRequestFromRepo = _repository.GetRequestById(Id);
+                if (updateRequestFromRepo == null)
+                {
+                    return NotFound();
+                }
+                _mapper.Map(updateRequest, updateRequestFromRepo);
+                _repository.UpdateRequest(updateRequestFromRepo);
+                _repository.SaveChanges();
+                return NoContent();
+            }*/
+
+
 
         // POST: api/Requests
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for
         // more details see https://aka.ms/RazorPagesCRUD.
-     
+
+
+        // POST: api/Requests
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public ActionResult<Request> CreateRequest(Request createRequest)
+        {
+            _repository.CreateRequest(createRequest);
+            _repository.SaveChanges();
+
+
+            //var email = createRequest.CreatedEmp.EmailId;
+            var fromAddress = new MailAddress("fromAddress", "My Name");
+            var toAddress = new MailAddress("toAddress", "Mr Test");
+            const string fromPassword = "password";
+            const string subject = "Request";
+            const string body = "Request Created!";
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword),
+                Timeout = 20000
+            };
+
+            using (var message = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = subject,
+                Body = body
+            })
+            {
+
+                smtp.Send(message);
+            }
+            return createRequest;
+        }
+
+        // DELETE: api/Requests/5
+        [HttpDelete("{id}")]
+        public ActionResult<Request> DeleteRequest(int Id)
+        {
+
+            var requestFromRepo = _repository.GetRequestById(Id);
+            if (requestFromRepo == null)
+            {
+                return NotFound();
+            }
+            _repository.DeleteRequest(requestFromRepo);
+            _repository.SaveChanges();
+            return NoContent();
+
+        }
+
+
     }
 }
