a89284a729bd0622cbc0c8616d48348befe33828
AnjaliK-123 <68889051+AnjaliK-123@users.noreply.github.com>
2020-08-13 15:10:26 +0530

    request

--- UserRegistration2/Services/Implementations/SqlRequestRepo.cs
+++ UserRegistration2/Services/Implementations/SqlRequestRepo.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using UserRegistration2.Models;
 using System;
 using System.Collections.Generic;
@@ -6,124 +6,121 @@ using System.Linq;
 using System.Net;
 using System.Net.Mail;
 using System.Threading.Tasks;
+using ServiceRequestManagment.Models;
+using UserRegistration1.Models;
 
 namespace UserRegistration2.Services.Implementations
 {
     public class SqlRequestRepo : IRequestRepo
     {
-        private readonly SRMContext _context;
-        public SqlRequestRepo(SRMContext context)
+      //  private readonly SRMContext _context;
+        private readonly IEmailSender emailSender;
+        //public SqlRequestRepo(SRMContext context)
+        //{
+        //    _context = context;
+        //}
+
+
+        public SqlRequestRepo(IEmailSender emailSender)
         {
-            _context = context;
+            this.emailSender = emailSender;
         }
-    /*    public void CreateRequest(Request request)
-        {
-           
-        }*/
 
         public void CreateRequest(Request request)
         {
+            var context = new SRMContext();
             if (request == null)
             {
                 throw new ArgumentNullException(nameof(request));
             }
-              _context.Request.Add(request);
-            
-              
-          
-            var email1 = _context.Employees.FirstOrDefault(e => e.Id == request.CreatedEmpId).EmailId;
-           var AdminEmail = _context.Employees.FirstOrDefault(e => e.DepartmentId == request.DepartmentId && e.RoleId == (_context.Roles.FirstOrDefault(r => r.Role1.Equals("Admin")).Id)).EmailId;
-            var fromAddress = new MailAddress("anjalikhadake888@gmail.com", "My Name");
-            var toAddress1 = new MailAddress((email1).ToString());
-            var toAddress2 = new MailAddress((AdminEmail).ToString());
-            const string fromPassword = "password";
-            string subject = "Service Request";
-            string body = "Request Created!" +
+           context.Request.Add(request);
+           context.SaveChanges();
+
+
+            var email1 = context.Employees.FirstOrDefault(e => e.Id == request.CreatedEmpId).EmailId;
+           var AdminEmail = context.Employees.FirstOrDefault(e => e.DepartmentId == request.DepartmentId && e.RoleId == (context.Roles.FirstOrDefault(r => r.Role1.Equals("Admin")).Id)).EmailId;
+            //var fromAddress = new MailAddress("anjalikhadake888@gmail.com", "My Name");
+            //var toAddress1 = new MailAddress((email1).ToString());
+            //var toAddress2 = new MailAddress((AdminEmail).ToString());
+            //const string fromPassword = "password";
+            string sub = "Service Request";
+            string content = "Request Created!" +
                             "\nRequest Id: " + request.Id +
-                                "\nRequest Created by: " + _context.Employees.FirstOrDefault(e => e.Id == request.CreatedEmpId).FirstName +
-                                "\nDepartment: " + _context.Department.FirstOrDefault(d => d.Id == request.DepartmentId).Name +
-                                 "\nCategory: " + _context.Category.FirstOrDefault(c => c.Id == request.CategoryId).Name +
-                                  "\nSubcategory: " + _context.Category.FirstOrDefault(s => s.Id == request.SubCategoryId).Name +
+                                "\nRequest Created by: " + context.Employees.FirstOrDefault(e => e.Id == request.CreatedEmpId).FirstName +
+                                "\nDepartment: " + context.Department.FirstOrDefault(d => d.Id == request.DepartmentId).Name +
+                                 "\nCategory: " + context.Category.FirstOrDefault(c => c.Id == request.CategoryId).Name +
+                                  "\nSubcategory: " + context.Category.FirstOrDefault(s => s.Id == request.SubCategoryId).Name +
                                   "\nTitle :" + request.Title +
                                   "\nSummary :" + request.Summary;
 
 
 
-            var smtp = new SmtpClient
-            {
-                Host = "smtp.gmail.com",
-                Port = 587,
-                EnableSsl = true,
-                DeliveryMethod = SmtpDeliveryMethod.Network,
-                UseDefaultCredentials = false,
-                Credentials = new NetworkCredential(fromAddress.Address, fromPassword),
-                Timeout = 20000
+            
 
-            };
-
-            using (var message1 = new MailMessage(fromAddress, toAddress1)
-            {
-                Subject = subject,
-                Body = body
-            })
-            using (var message2 = new MailMessage(fromAddress, toAddress2)
-            {
-                Subject = subject,
-                Body = body
-            })
-            {
-
-                smtp.Send(message1);
-                smtp.Send(message2);
-            }
+            var message = new Message(new string[] { email1, AdminEmail }, sub, content);
+            emailSender.SendEmail(message);
+           
         }
+        //catch (Exception ex)
+        //{
+        //    Console.Error.WriteLine(ex.ToString());
+        //}
+
+
 
         public void DeleteRequest(Request request)
         {
+            var context = new SRMContext();
             if (request == null)
             {
                 throw new ArgumentNullException(nameof(request));
             }
-            _context.Request.Remove(request);
-           
+            context.Request.Remove(request);
+
         }
 
-      /*  public IEnumerable<Request> GetRequest()
+        //public IEnumerable<Request> GetRequest()
+        // {
+
+        //     return _context.Request
+        //     .Include(stat => stat.Status)
+        //     .Include(dept => dept.Department)
+        //     .Include(cat => cat.Category)
+        //     .Include(cat => cat.SubCategory)
+        //     .ToList();
+
+        // }
+        public List<Request> GetRequests()
         {
-            return _context.Request
-                .Include(stat => stat.Status)
-                .Include(dept => dept.Department)
-                .Include(cat => cat.Category)
-                .Include(cat => cat.SubCategory)
-                .ToList();
-        }*/
-         public List<Request> GetRequests()
-        {
-           
-        var _context = new SRMContext();
-        return _context.Request.ToList();
+
+            var context = new SRMContext();
+        return context.Request.ToList();
         }
 
         public Request GetRequestById(int Id)
         {
-            var request = _context.Request
+            var context = new SRMContext();
+            var request = context.Request
            .Include(stat => stat.Status)
             .Include(dept => dept.Department)
           .Include(cat => cat.Category)
                                     .Where(req => req.Id == Id).FirstOrDefault();
+           
             return request;
         }
 
-        public bool SaveChanges()
-        {
-            return (_context.SaveChanges() >= 0);
-        }
+        //public bool SaveChanges()
+        //{
+            
+        //    return (_context.SaveChanges() >= 0);
+        //}
 
         public void UpdateRequest(Request request)
         {
             
         }
 
-      
+       
     }
 }
+
